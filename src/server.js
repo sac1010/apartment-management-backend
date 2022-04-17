3208597918f725400aff45bc9d4383b9d9326c4e
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-04-17 14:26:44 +0530

    heroku

--- src/server.js
+++ src/server.js
@@ -10,9 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
 
-// app.get("/", (req, res)=>{
-//     res.send("hello world!")
-// })
+app.get("/", (req, res)=>{
+    res.send("hello world!")
+})
 app.use("/auth", authController)
 
 const port = process.env.PORT || 3001
